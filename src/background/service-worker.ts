36ede6c8d20f2ec28b8e9318dc91af92e717d2c2
jlake <jim@blueskylabs.com>
2026-05-16 09:14:15 -0700

    game preset

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -86,6 +86,10 @@ async function handleInitialized(
     ? { name: gamePreset, config: resolveConfig(syncData, gamePreset) }
     : getActiveConfig(syncData);
 
+  if (gamePreset) {
+    await chrome.storage.sync.set({ ACTIVE_GP_CONF: name });
+  }
+
   if (isEnabled && config) {
     sendResponse({
       source: MSG_SOURCE,
