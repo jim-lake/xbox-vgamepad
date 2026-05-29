e856a843caf3fe97ab73eb4412def8a0808da798
jlake <jim@blueskylabs.com>
2026-05-29 09:20:45 -0700

    icon color in light mode

--- src/components/popup/gamepad-tabs.tsx
+++ src/components/popup/gamepad-tabs.tsx
@@ -48,8 +48,8 @@ const styles = StyleSheet.create({
   },
   tabTextActive: { color: 'var(--text-primary)' },
   gearSlot: { justifyContent: 'center', alignItems: 'center' },
-  gearIcon: { width: '1.6rem', height: '1.6rem', opacity: 0.6 },
-  gearIconActive: { width: '1.6rem', height: '1.6rem', opacity: 1 },
+  gearIcon: { width: '1.6rem', height: '1.6rem', opacity: 0.6, filter: 'var(--icon-filter)' },
+  gearIconActive: { width: '1.6rem', height: '1.6rem', opacity: 1, filter: 'var(--icon-filter)' },
 });
 
 interface Props {
